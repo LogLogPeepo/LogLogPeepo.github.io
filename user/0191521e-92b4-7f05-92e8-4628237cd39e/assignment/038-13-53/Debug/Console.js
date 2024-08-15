49a42da2af520c725e9a73f3fec2b37f434f0b8a
M44 <50850936+M3KAI5ER44@users.noreply.github.com>
2024-08-15 14:07:21 -0400

    i sure hope this works

---
+++ user/0191521e-92b4-7f05-92e8-4628237cd39e/assignment/038-13-53/Debug/Console.js
@@ -0,0 +1,7 @@
+var realConsoleLog = console.log;
+console.log = function () {
+    var message = [].join.call(arguments, " ");
+    // Display the message somewhere... (jQuery example)
+    $(".output").text(message);
+    realConsoleLog.apply(console, arguments);
+};
